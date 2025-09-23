d175ca1a4e35213858353e64208cb4f09c38806a
NgoniNed <ressned@gmail.com>
2025-09-23 06:34:28 +0200

    removed default files and assorciated links

--- HumanResource/Startup.cs
+++ HumanResource/Startup.cs
@@ -31,7 +31,6 @@ namespace HumanResource
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton(new CommunicationService(departmentName));
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
